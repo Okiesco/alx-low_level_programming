0672b217df3b14a4cb21b176bb0d3c2f7da47ebb
Okiesco <fj246281@gmail.com>
2022-07-08 22:31:02 -0700

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,20 @@
-i#include "main.h"
-
+#include "main.h"
 #include <ctype.h>
-
-
-
 /**
  * _isupper - checks for uppercase letters
  *
- *  @c: parametr to be printed
+ * @c: parametr to be printed
  *
- *  Return: 0 or 1 depending on the condition
+ * Return: 0 or 1 depending on the condition
  */
 int _isupper(int c)
 {
-	if (isupper(c))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
