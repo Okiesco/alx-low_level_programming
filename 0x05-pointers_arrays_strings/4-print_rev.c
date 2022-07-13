1193f5fea109a960d7cb58dc416c8009eb06680f
Okiesco <fj246281@gmail.com>
2022-07-12 22:06:19 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - entry point
+ *  @s: string input
+ *  Return: void
+ */
+void print_rev(char *s)
+{
+int fcounter = 0;
+int i, n;
+for (i = 0; s[i] != '\0'; i++)
+{
+fcounter++;
+}
+for (n = (fcounter - 1); n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
