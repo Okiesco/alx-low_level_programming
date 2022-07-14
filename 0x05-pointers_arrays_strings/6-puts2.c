44f2c55ff6184b0097ac6dd4a5c0f204fc7d0f2f
Okiesco <fj246281@gmail.com>
2022-07-14 07:45:18 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,22 @@
 #include "main.h"
 /**
- * puts2 - entry pointputs2 - entry pointput
+ * puts2 - entry point
+ * @str: string input
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
+{
+_putchar(str[i]);
+} else
+{
+continue;
+}
+}
+_putchar('\n');
+}
+
