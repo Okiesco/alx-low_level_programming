289e1c633a125a4fea90b692ad6d8e4a0cef0d58
Okiesco <fj246281@gmail.com>
2022-07-14 08:25:25 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - entry point
+ * @a: array name
+ * @n: nb of elmts to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != (n - 1))
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+
